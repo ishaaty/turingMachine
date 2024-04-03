d85de2f755b668f2ebb781ea7e8b17d650f18c75
Isha Tyagi <ishatyagi888@gmail.com>
2024-04-02 21:48:47 -0400

    made state class

---
+++ State.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+abstract class State {
+
+    public String name = "";
+    public ArrayList<ArrayList<String>> transitions = new ArrayList<>();
+    public ArrayList<String> finalStates = new ArrayList<>();
+    
+    public State(String name, ArrayList<ArrayList<String>> transitions, ArrayList<String> finalStates){
+        this.name = name;
+        this.transitions = transitions;
+        this.finalStates = finalStates;
+    }
+
+    public boolean isFinalState(){
+
+        for (int i = 0; i < finalStates.size(); i++){
+            if (finalStates.get(i) == name){
+                return true;
+            }
+        }
+        return false;
+
+    }
+
+}
